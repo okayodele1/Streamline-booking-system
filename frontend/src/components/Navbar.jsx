8b72a98d2390b974da0dbc46365d4e786b818402
okayodele1 <okunadeayodele1@gmail.com>
2024-04-15 16:12:30 +0100

    Appointment Modal added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,19 +3,39 @@ import { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../assets/salon.png"; 
 import { Link } from "react-router-dom";
-
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import TextField from "@mui/material/TextField";
 import './Navbar.css';
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  height: 600,
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #FF1260",
+  boxShadow: 24,
+  p: 9,
+  '@media (max-width: 768px)': {
+   height:500,
+   width:270
+  },
+};
 
 export default function NewNavbar() {
   const [showNavbar, setShowNavbar] = useState(false);
-  
+  const [open, setOpen] = useState(false);
 
   const handleShowNavbar = () => {
     setShowNavbar(!showNavbar);
   };
 
   const handleOpen = () => setOpen(true);
-  
+  const handleClose = () => setOpen(false);
 
   return (
     <nav className="new-navbar">
@@ -53,8 +73,34 @@ export default function NewNavbar() {
             </li>
           </ul>
         </div>
-    
-
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <HighlightOffIcon onClick={handleClose} className="signup-close"/>
+            <h3 className="modal-header">Book an Appointment</h3>
+            <p className="modal-sub-header">Fill in the details below to schedule your appointment:</p>
+            <div className="top-bar-input">
+              <TextField fullWidth label="Full Name" variant="outlined" />
+            </div>
+            <div className="top-bar-input">
+              <TextField fullWidth label="Email" variant="outlined" />
+            </div>
+            <div className="top-bar-input">
+              <TextField fullWidth label="Phone Number" variant="outlined" />
+            </div>
+            <div className="top-bar-input">
+              <TextField fullWidth label="" variant="outlined" type="date" />
+            </div>
+            <div className="top-bar-input">
+              <TextField fullWidth label="" variant="outlined" type="time" />
+            </div>
+            <button className="modal-button" onClick={handleClose}>Book Appointment</button>
+          </Box>
+        </Modal>
       </div>
     </nav>
   );
