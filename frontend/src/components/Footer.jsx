7e64f35a8bd0c681d32addb48d2f137e61d8458c
okayodele1 <okunadeayodele1@gmail.com>
2024-04-15 16:23:20 +0100

    Additional Css Update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,14 +10,35 @@ function Footer() {
         <div className="footer">
           {/* <img src="" alt="" className="footer-logo" /> */}
           <p className="footer-heading">Contact Us</p>
-          <p className="footer-list">
-            Beauty Salon,
-            123 High Street, Chelsea,
+          <span className="footer-list">
+           Chelsea,
             London, SW1X 7RN
-          </p>
+          </span>
           <p className="footer-list">07 321 546 8764</p>
           <p className="footer-list">info@beautysalon.co.uk</p>
           <p className="footer-list">www.beautysalon.co.uk</p>
+         
+        </div>
+        <div className="footer">
+          <p className="footer-heading">Quick Links</p>
+          <p className="footer-heading">Opening Hours</p>
+          <p className="footer-list">Mon - Fri 9am-9pm</p>
+          <p className="footer-list">Sat 8am -10pm</p>
+          <p className="footer-list">Sun 9am- 9pm</p>
+        </div>
+        <div className="footer">
+          <p className="footer-heading">Terms and Conditions</p>
+          <p className="footer-list">Privacy Policy</p>
+          <p className="footer-list">FAQs</p>
+          <p className="footer-list">Cancel An Appointment</p>
+      
+        </div>
+        <div className="footer-icons">
+        <p className="footer-heading">Follow us</p>
+          <FacebookIcon />
+          <InstagramIcon />
+          <XIcon />
+          <YouTubeIcon />
         </div>
         <div className="footer">
           <p className="footer-heading">Quick Links</p>
@@ -26,18 +47,8 @@ function Footer() {
           <p className="footer-list">Gallery</p>
           <p className="footer-list">Contact Us</p>
         </div>
-        <div className="footer">
-          <p className="footer-heading">Terms and Conditions</p>
-          <p className="footer-list">Privacy Policy</p>
-          <p className="footer-list">FAQs</p>
-        </div>
-        <div className="footer-icons">
-          <FacebookIcon />
-          <InstagramIcon />
-          <XIcon />
-          <YouTubeIcon />
-        </div>
       </div>
+
       <p className="copyright">© 2024 Beauty Salon. All Rights Reserved</p>
     </footer>
   );
