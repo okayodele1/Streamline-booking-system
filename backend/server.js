9f2e61d6d059132126b84c0f6112c6dfd69c5ec9
okayodele1 <okunadeayodele1@gmail.com>
2024-04-29 18:45:26 +0100

    Use Auth and Appointment Routes in Server,js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.js";
+
+ import appointmentRoutes from "./routes/appointment.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
+app.use("/api/appointment", appointmentRoutes);
 
 app.listen(8800, () => {
   console.log("Connected to backend999");
